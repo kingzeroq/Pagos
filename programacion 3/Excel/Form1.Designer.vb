d5e47d73fb9bfe2a2ac9cb2747ee8548862588df
kingzeroq <90411555+kingzeroq@users.noreply.github.com>
2024-10-17 11:04:10 -0600

    a

---
+++ programacion 3/Excel/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MonthCalendar1 = New MonthCalendar()
+        ListBox1 = New ListBox()
+        CMes = New ComboBox()
+        Cpagos = New ComboBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        BCancelar = New Button()
+        BGuardar = New Button()
+        BAgregar = New Button()
+        BSalir = New Button()
+        TxtNombre = New TextBox()
+        TxtNumero = New TextBox()
+        BExcel = New Button()
+        SuspendLayout()
+        ' 
+        ' MonthCalendar1
+        ' 
+        MonthCalendar1.Location = New Point(503, 63)
+        MonthCalendar1.Name = "MonthCalendar1"
+        MonthCalendar1.TabIndex = 0
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.Location = New Point(30, 332)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(364, 44)
+        ListBox1.TabIndex = 1
+        ' 
+        ' CMes
+        ' 
+        CMes.FormattingEnabled = True
+        CMes.Location = New Point(630, 321)
+        CMes.Name = "CMes"
+        CMes.Size = New Size(90, 28)
+        CMes.TabIndex = 2
+        ' 
+        ' Cpagos
+        ' 
+        Cpagos.FormattingEnabled = True
+        Cpagos.Location = New Point(492, 319)
+        Cpagos.Name = "Cpagos"
+        Cpagos.Size = New Size(107, 28)
+        Cpagos.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(501, 279)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(54, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "Estado"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(631, 282)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(36, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "Mes"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 50)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(64, 20)
+        Label3.TabIndex = 8
+        Label3.Text = "Nombre"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 99)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(119, 20)
+        Label4.TabIndex = 9
+        Label4.Text = "Numero de Casa"
+        ' 
+        ' BCancelar
+        ' 
+        BCancelar.Location = New Point(195, 177)
+        BCancelar.Name = "BCancelar"
+        BCancelar.Size = New Size(96, 47)
+        BCancelar.TabIndex = 10
+        BCancelar.Text = "Cancelar"
+        BCancelar.UseVisualStyleBackColor = True
+        ' 
+        ' BGuardar
+        ' 
+        BGuardar.Location = New Point(77, 255)
+        BGuardar.Name = "BGuardar"
+        BGuardar.Size = New Size(96, 47)
+        BGuardar.TabIndex = 11
+        BGuardar.Text = "Guardar"
+        BGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' BAgregar
+        ' 
+        BAgregar.Location = New Point(77, 177)
+        BAgregar.Name = "BAgregar"
+        BAgregar.Size = New Size(96, 47)
+        BAgregar.TabIndex = 12
+        BAgregar.Text = "Agregar"
+        BAgregar.UseVisualStyleBackColor = True
+        ' 
+        ' BSalir
+        ' 
+        BSalir.Location = New Point(195, 255)
+        BSalir.Name = "BSalir"
+        BSalir.RightToLeft = RightToLeft.Yes
+        BSalir.Size = New Size(96, 47)
+        BSalir.TabIndex = 13
+        BSalir.Text = "Salir"
+        BSalir.UseVisualStyleBackColor = True
+        ' 
+        ' TxtNombre
+        ' 
+        TxtNombre.Location = New Point(123, 47)
+        TxtNombre.Name = "TxtNombre"
+        TxtNombre.Size = New Size(97, 27)
+        TxtNombre.TabIndex = 14
+        ' 
+        ' TxtNumero
+        ' 
+        TxtNumero.Location = New Point(153, 96)
+        TxtNumero.Name = "TxtNumero"
+        TxtNumero.Size = New Size(97, 27)
+        TxtNumero.TabIndex = 15
+        ' 
+        ' BExcel
+        ' 
+        BExcel.Location = New Point(353, 174)
+        BExcel.Name = "BExcel"
+        BExcel.Size = New Size(93, 41)
+        BExcel.TabIndex = 16
+        BExcel.Text = "ir a excel"
+        BExcel.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(BExcel)
+        Controls.Add(TxtNumero)
+        Controls.Add(TxtNombre)
+        Controls.Add(BSalir)
+        Controls.Add(BAgregar)
+        Controls.Add(BGuardar)
+        Controls.Add(BCancelar)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Cpagos)
+        Controls.Add(CMes)
+        Controls.Add(ListBox1)
+        Controls.Add(MonthCalendar1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents CMes As ComboBox
+    Friend WithEvents Cpagos As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents BCancelar As Button
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents BAgregar As Button
+    Friend WithEvents BSalir As Button
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents TxtNumero As TextBox
+    Friend WithEvents BExcel As Button
+
+End Class
---
+++ programacion 3/Excel/Form1.vb
@@ -0,0 +1,121 @@
+﻿Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Services
+Imports Google.Apis.Sheets.v4
+Imports Google.Apis.Sheets.v4.Data
+Imports Google.Apis.Util.Store
+Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+    ' Variables de credenciales
+    Private ReadOnly credentialFilePath As String = "C:\Users\danie\OneDrive\Escritorio\programacion 3\pagos-436715-cba82996c247.json"
+    Private service As SheetsService
+
+    ' Declarar constantes como campos Static ReadOnly
+    Private Shared ReadOnly meses As String() = New String() {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"}
+    Private Shared ReadOnly estados As String() = New String() {"Pago", "No Pago"}
+
+    ' Método para inicializar el servicio de Google Sheets
+    Private Sub InitializeService()
+        Dim credential As GoogleCredential
+        Using stream = New FileStream(credentialFilePath, FileMode.Open, FileAccess.Read)
+            credential = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.Spreadsheets)
+        End Using
+
+        service = New SheetsService(New BaseClientService.Initializer() With {
+            .HttpClientInitializer = credential,
+            .ApplicationName = "Aplicación Pagos"
+        })
+    End Sub
+
+    ' Método para guardar datos en Google Sheets
+    Private Sub GuardarDatosEnGoogleSheets()
+        Dim nombre As String = TxtNombre.Text
+        Dim numeroCasa As String = TxtNumero.Text
+        Dim mes As String = Cpagos.SelectedItem.ToString() ' ComboBox de meses
+        Dim estado As String = Cpagos.SelectedItem.ToString() ' Cambiar a Cpagos para que tome el estado
+
+        ' Verificar campos
+        If String.IsNullOrWhiteSpace(nombre) Or String.IsNullOrWhiteSpace(numeroCasa) Or String.IsNullOrWhiteSpace(mes) Then
+            MessageBox.Show("Por favor, complete todos los campos.")
+            Return
+        End If
+
+        ' Obtener la ID de la hoja según el mes
+        Dim spreadsheetId As String = "1QE9DEZtOCLzr9we18hkw3s3ilX4cyrdnnoa1Tqus4x8" ' ID de la hoja de cálculo
+        Dim range As String = $"{mes}!A:C" ' Rango de columnas a guardar
+
+        ' Obtener los valores actuales
+        Dim request As SpreadsheetsResource.ValuesResource.GetRequest = service.Spreadsheets.Values.Get(spreadsheetId, range)
+        Dim response As ValueRange = request.Execute()
+        Dim values As IList(Of IList(Of Object)) = response.Values
+
+        ' Agregar nuevos datos
+        If values Is Nothing OrElse values.Count = 0 Then
+            values = New List(Of IList(Of Object))()
+        End If
+        values.Add(New List(Of Object) From {nombre, numeroCasa, estado})
+
+        ' Actualizar la hoja
+        Dim updateRequest = New SpreadsheetsResource.ValuesResource.UpdateRequest(service, New ValueRange With {
+            .Values = values,
+            .MajorDimension = "ROWS"
+        }, spreadsheetId, range)
+
+        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED
+        updateRequest.Execute()
+
+        MessageBox.Show("Datos guardados exitosamente.")
+    End Sub
+
+    ' Evento de clic del botón de guardar
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        GuardarDatosEnGoogleSheets()
+    End Sub
+
+    ' Evento de clic del botón de Excel
+    Private Sub BExcel_Click(sender As Object, e As EventArgs) Handles BExcel.Click
+        Try
+            Dim url As String = "https://sheets.google.com"
+            Dim processInfo As New ProcessStartInfo(url) With {
+                .UseShellExecute = True ' Asegura que se abra en el navegador
+            }
+            Process.Start(processInfo)
+        Catch ex As Exception
+            MessageBox.Show($"Error al abrir Google Sheets: {ex.Message}")
+        End Try
+    End Sub
+
+    ' Evento de clic del botón Agregar (para agregar los datos al ListBox)
+    Private Sub BAgregar_Click(sender As Object, e As EventArgs) Handles BAgregar.Click
+        Dim nombre As String = TxtNombre.Text
+        Dim numeroCasa As String = TxtNumero.Text
+        Dim mes As String = Cpagos.SelectedItem?.ToString() ' ComboBox de meses
+        Dim estado As String = Cpagos.SelectedItem?.ToString() ' Cambiar a Cpagos para que tome el estado
+
+        ' Verificar campos
+        If String.IsNullOrWhiteSpace(nombre) Or String.IsNullOrWhiteSpace(numeroCasa) Or String.IsNullOrWhiteSpace(mes) Or String.IsNullOrWhiteSpace(estado) Then
+            MessageBox.Show("Por favor, complete todos los campos.")
+            Return
+        End If
+
+        ' Agregar los datos al ListBox
+        ListBox1.Items.Add($"Nombre: {nombre}, Casa: {numeroCasa}, Mes: {mes}, Estado: {estado}")
+    End Sub
+
+    ' Evento de carga del formulario
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeService()
+
+        ' Inicializar ComboBox de meses y estados
+        Cpagos.Items.AddRange(meses)
+        Cpagos.Items.AddRange(estados) ' Cargamos opciones de estado
+    End Sub
+
+    ' Validar que TxtNumero solo acepte números
+    Private Sub TxtNumero_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtNumero.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
